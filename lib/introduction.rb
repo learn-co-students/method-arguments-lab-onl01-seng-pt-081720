9614894e166cf5f75ddd839e30917e29b0477648
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-08-28 01:23:15 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
-def introduction name 
-  puts "Hi, my name is Dan and I am learning to program Ember.js."
-
-  end
-name = Josh
-
+require 'pry' 
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+ 
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
