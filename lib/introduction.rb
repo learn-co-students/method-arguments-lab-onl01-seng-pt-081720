7e5c405e5938b8df45c48feda44dc589e97eccf6
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-08-28 00:53:39 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction name
   puts "Hi, my name is Dan and I am learning to program Ember.js."
 
   end
-name = validates_exclusion_of
+name = Josh
 
 def introduction(name)
   puts "Hi, my name is #{name}."
